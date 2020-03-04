3ef68f368a44b15d196a8448208882a0c5f056d2
KorKozan <kozankor@gmail.com>
2020-03-04 10:33:16 +0200

    Added support for png tiles (v1.1.0).

--- tools/mkfw/main.c
+++ tools/mkfw/main.c
@@ -1,7 +1,34 @@
 /***
  *
  * This tool is a replacement for the mkfw tool at: https://github.com/OtherCrashOverride/odroid-go-firmware
- * This code is in the Public Domain.
+ * This code is in the Public Domain:
+ *
+ * This is free and unencumbered software released into the public domain.
+ *
+ * Anyone is free to copy, modify, publish, use, compile, sell, or
+ * distribute this software, either in source code form or as a compiled
+ * binary, for any purpose, commercial or non-commercial, and by any
+ * means.
+ *
+ * In jurisdictions that recognize copyright laws, the author or authors
+ * of this software dedicate any and all copyright interest in the
+ * software to the public domain. We make this dedication for the benefit
+ * of the public at large and to the detriment of our heirs and
+ * successors. We intend this dedication to be an overt act of
+ * relinquishment in perpetuity of all present and future rights to this
+ * software under copyright law.
+ *
+ * THE SOFTWARE IS PROVIDED "AS IS", WITHOUT WARRANTY OF ANY KIND,
+ * EXPRESS OR IMPLIED, INCLUDING BUT NOT LIMITED TO THE WARRANTIES OF
+ * MERCHANTABILITY, FITNESS FOR A PARTICULAR PURPOSE AND NONINFRINGEMENT.
+ * IN NO EVENT SHALL THE AUTHORS BE LIABLE FOR ANY CLAIM, DAMAGES OR
+ * OTHER LIABILITY, WHETHER IN AN ACTION OF CONTRACT, TORT OR OTHERWISE,
+ * ARISING FROM, OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR
+ * OTHER DEALINGS IN THE SOFTWARE.
+ *
+ * For more information, please refer to <http://unlicense.org/>
+ *
+ *
  * Alternatively, if Public Domain is not feasible in your jurisdiction or if it doesn't meet your needs,
  * you can use it under the terms and conditions of the Zero Clause BSD license:
  *
@@ -20,6 +47,8 @@
 #ifdef __linux__
 #   define _DEFAULT_SOURCE
 #endif
+
+#include "upng/upng.h"
 
 #include <stdlib.h>
 #include <stdio.h>
@@ -75,13 +104,13 @@ typedef struct odroid_partition_t
 static_assert (sizeof(odroid_partition_t) == 28, "The partition header should be exactly 28 bytes");
 
 // app return codes
-enum return_code_t
+typedef enum
 {
     RET_OK              =  0, // all OK
     RET_INVALID_PARAMS  = -1, // parameters are invalid
     RET_READ_ERROR      = -2, // I/O error while reading from a file
     RET_WRITE_ERROR     = -3, // I/O error while writing to the output file
-};
+} return_code_t;
 
 // values for predefined partition types
 enum partition_type_t
@@ -106,7 +135,7 @@ enum partition_subtype_t
 #define RETURN(x) do { result = (x); goto cleanup; } while (false)
 
 // current version
-static const char* kVersion = "1.0.0 (20200222)";
+static const char* kVersion = "1.1.0 (20200304)";
 
 static const struct option kLongOptions[] = {
 //   NAME       ARGUMENT           FLAG  SHORTNAME
@@ -122,7 +151,9 @@ enum
 {   // constants
     kHeaderSize = 24,
     kFirmwareDescriptionSize = 40,
-    kTileSize = 86 * 48 * 2,
+    kTileWidth = 86,
+    kTileHeight = 48,
+    kTileByteSize = kTileWidth * kTileHeight * sizeof(uint16_t),
     kBufferSize = 10 * 1024 * 1024,
 };
 
@@ -139,7 +170,7 @@ static char sFirmwareDescription[kFirmwareDescriptionSize];
 // 8KB: 86x48 pixels, 16bpp
 
 // 10 MB for the buffer, make sure this is at least as large as kTileSize
-static_assert (kBufferSize >= kTileSize, "The buffer should be at least as large as the tile size");
+static_assert (kBufferSize >= kTileByteSize, "The buffer should be at least as large as the tile size");
 
 static uint8_t sBuffer[kBufferSize];
 
@@ -149,6 +180,292 @@ static bool writeAndCrc(const void* ptr, size_t size, FILE* stream, uint32_t* cr
 {
     *crc = crc32(*crc, ptr, size);
     return (fwrite(ptr, size, 1, stream) == 1);
+}
+
+static bool endsWith(const char* str, const char* suffix)
+{
+    const size_t suffixLen = strlen(suffix);
+    const size_t strLen = strlen(str);
+    return (strLen >= suffixLen) && (strcmp(str + strLen - suffixLen, suffix) == 0);
+}
+
+static return_code_t handleUpngError(upng_error err, const char* tileFileName)
+{
+    switch (err) {
+    case UPNG_EOK:
+        return RET_OK;
+    case UPNG_ENOMEM:           // Out of memory
+        fprintf(stderr, "Out of memory while decoding tile png: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    case UPNG_ENOTFOUND:        // Resource not found
+        fprintf(stderr, "Unable to read tile png: %s\n", tileFileName);
+        return RET_READ_ERROR;
+    case UPNG_ENOTPNG:          // Invalid file header (not a PNG image)
+        fprintf(stderr, "Invalid png header (maybe not a png?) for the tile: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    case UPNG_EMALFORMED:       // PNG image data does not follow spec and is malformed
+        fprintf(stderr, "Malformed tile png: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    case UPNG_EUNSUPPORTED:     // PNG image data is well-formed but not supported by uPNG
+        fprintf(stderr, "Unsupported format for the tile png: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    case UPNG_EUNINTERLACED:    // image interlacing is not supported
+        fprintf(stderr, "Image interlacing no supported for the tile png: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    case UPNG_EUNFORMAT:        // image color format is not supported
+        fprintf(stderr, "Image color format is not supported for the tile png: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    case UPNG_EPARAM:           // invalid parameter to method call
+        fprintf(stderr, "Internal error while decoding tile png: %s\n", tileFileName);
+        return RET_INVALID_PARAMS;
+    }
+    fprintf(stderr, "Uknown error while decoding tile png: %s\n", tileFileName);
+    return RET_INVALID_PARAMS;
+}
+
+#define RGB565(r, g, b) (((r) << 11) | ((g) << 5) | (b))
+
+static return_code_t decodePng(const char* tileFileName, const bool quiet)
+{
+    return_code_t result = RET_OK;
+
+    upng_t* upng = upng_new_from_file(tileFileName);
+    if (!upng) {
+        fprintf(stderr, "Unable to open tile file: %s\n", tileFileName);
+        return RET_READ_ERROR;
+    }
+    result = handleUpngError(upng_header(upng), tileFileName);
+    if (result != RET_OK) {
+        goto cleanup;
+    }
+    if (upng_get_width(upng) != kTileWidth) {
+        fprintf(stderr, "Tile width should be %d, png width is: %u", kTileWidth, upng_get_width(upng));
+        RETURN (RET_INVALID_PARAMS);
+    }
+    if (upng_get_height(upng) != kTileHeight) {
+        fprintf(stderr, "Tile height should be %d, png width is: %u", kTileHeight, upng_get_height(upng));
+        RETURN (RET_INVALID_PARAMS);
+    }
+    result = handleUpngError(upng_decode(upng), tileFileName);
+    if (result != RET_OK) {
+        goto cleanup;
+    }
+    const uint8_t* pngData = upng_get_buffer(upng);
+    const uint16_t* rawDataEnd = (uint16_t*)(sBuffer + kTileByteSize);
+    const upng_format fmt = upng_get_format(upng);
+    switch (fmt) {
+    case UPNG_BADFORMAT:        // unknown/unhandled format
+        fprintf(stderr, "Uknown or unsupport png format for tile file: %s\n", tileFileName);
+        RETURN (RET_INVALID_PARAMS);
+        break;
+    case UPNG_RGB8:             // 24-bit RGB
+    case UPNG_RGBA8:            // 32-bit RGBA
+        if (!quiet) {
+            printf("Tile png format is RGB%s8\n", ((fmt == UPNG_RGB8) ? "" : "A"));
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            const uint8_t a = (fmt == UPNG_RGB8) ? 0xFF : pngData[3];
+            const uint8_t r = (pngData[0] * a / 0xFF) >> 3;
+                  uint8_t g = ((pngData[1] * a / 0xFF) >> 3) << 1;
+                          g |= g >> 5;
+            const uint8_t b = (pngData[2] * a / 0xFF) >> 3;
+            const uint16_t rgb565 = RGB565(r, g, b);
+            *rawData = htole16(rgb565);
+
+            ++rawData;
+            pngData += upng_get_bpp(upng) / 8;
+        }
+        break;
+    case UPNG_RGB16:            // 48-bit RGB
+    case UPNG_RGBA16:           // 64-bit RGBA
+        if (!quiet) {
+            printf("Tile png format is RGB%s16\n", ((fmt == UPNG_RGB8) ? "" : "A"));
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            const uint16_t* pngData16 = (uint16_t*)pngData;
+            const uint16_t a = (fmt == UPNG_RGB16) ? 0xFFFF : pngData16[3];
+            const uint8_t r = (pngData16[0] * a / 0xFFFF) >> 11;
+                  uint8_t g = ((pngData[1] * a / 0xFFFF) >> 11) << 1;
+                          g |= g >> 5;
+            const uint8_t b = (pngData16[2] * a / 0xFFFF) >> 11;
+            const uint16_t hrgb565 = RGB565(r, g, b);
+            *rawData = htole16(hrgb565);
+
+            ++rawData;
+            pngData += upng_get_bpp(upng) / 8;
+        }
+        break;
+    case UPNG_LUMINANCE1:       // 1-bit Greyscale
+        if (!quiet) {
+            printf("Tile png format is 1-bit grayscale\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            *rawData = htole16(((*pngData >> 7) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 6) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 5) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 4) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 3) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 2) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 1) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 0) & 0x01) ? 0xFFFF : 0x0000);
+            ++rawData;
+
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE2:       // 2-bit Greyscale
+        if (!quiet) {
+            printf("Tile png format is 2-bit grayscale\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            static const uint16_t colors[4] = {0x0000, RGB565(10, 20, 10), RGB565(21, 43, 21), 0xffff};
+
+            *rawData = htole16(colors[(*pngData >> 6) & 0x03]);
+            ++rawData;
+            *rawData = htole16(colors[(*pngData >> 4) & 0x03]);
+            ++rawData;
+            *rawData = htole16(colors[(*pngData >> 2) & 0x03]);
+            ++rawData;
+            *rawData = htole16(colors[(*pngData >> 6) & 0x03]);
+            ++rawData;
+
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE4:       // 4-bit Greyscale
+        if (!quiet) {
+            printf("Tile png format is 4-bit grayscale\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            {
+                const uint8_t v = (*pngData >> 4) & 0x0F;
+                const uint8_t r = (v << 1) | (v >> 3);
+                const uint8_t g = (r << 1) | (r >> 4);
+                const uint8_t b = r;
+                const uint16_t hrgb565 = RGB565(r, g, b);
+                *rawData = htole16(hrgb565);
+                ++rawData;
+            }
+            {
+                const uint8_t v = (*pngData >> 0) & 0x0F;
+                const uint8_t r = (v << 1) | (v >> 3);
+                const uint8_t g = (r << 1) | (r >> 4);
+                const uint8_t b = r;
+                const uint16_t hrgb565 = RGB565(r, g, b);
+                *rawData = htole16(hrgb565);
+                ++rawData;
+            }
+
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE8:       // 8-bit Greyscale
+        if (!quiet) {
+            printf("Tile png format is 8-bit grayscale\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            const uint8_t v = (*pngData >> 4) & 0x0F;
+            const uint8_t r = (v << 1) | (v >> 3);
+            const uint8_t g = (r << 1) | (r >> 4);
+            const uint8_t b = r;
+            const uint16_t hrgb565 = RGB565(r, g, b);
+            *rawData = htole16(hrgb565);
+
+            ++rawData;
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE_ALPHA1: // 1-bit Greyscale w/ 1-bit Alpha
+        if (!quiet) {
+            printf("Tile png format is 1-bit grayscale w/ 1-bit alpha\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            *rawData = htole16(((*pngData >> 6) & 0x03) == 0x03 ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 4) & 0x03) == 0x03 ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 2) & 0x03) == 0x03 ? 0xFFFF : 0x0000);
+            ++rawData;
+            *rawData = htole16(((*pngData >> 0) & 0x03) == 0x03 ? 0xFFFF : 0x0000);
+            ++rawData;
+
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE_ALPHA2: // 2-bit Greyscale w/ 2-bit Alpha
+        if (!quiet) {
+            printf("Tile png format is 2-bit grayscale w/ 2-bit alpha\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            static const uint16_t colors[10] = {
+                0x0000, RGB565(3, 7, 3), RGB565(7, 14, 17), RGB565(10, 21, 10), RGB565(14, 28, 14),
+                0, RGB565(21, 43, 21), 0, 0, 0xFFFF
+            };
+
+            {
+                const uint8_t v = (*pngData >> 6) & 0x03;
+                const uint8_t a = (*pngData >> 4) & 0x03;
+                *rawData = htole16(colors[v * a]);
+                ++rawData;
+            }
+            {
+                const uint8_t v = (*pngData >> 2) & 0x03;
+                const uint8_t a = (*pngData >> 0) & 0x03;
+                *rawData = htole16(colors[v * a]);
+                ++rawData;
+            }
+
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE_ALPHA4: // 4-bit Greyscale w/ 4-bit Alpha
+        if (!quiet) {
+            printf("Tile png format is 4-bit grayscale w/ 4-bit alpha\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            const uint8_t v = (*pngData >> 4) & 0x07;
+            const uint8_t a = (*pngData >> 0) & 0x07;
+            const uint8_t c = ((v * a) << 1) / 0x07;
+            const uint8_t r = c | (c >> 4);
+            const uint8_t g = (r << 1) | (r >> 4);
+            const uint8_t b = r;
+            const uint16_t hrgb565 = RGB565(r, g, b);
+            *rawData = htole16(hrgb565);
+
+            ++rawData;
+            ++pngData;
+        }
+        break;
+    case UPNG_LUMINANCE_ALPHA8: // 8-bit Greyscale w/ 8-bit Alpha
+        if (!quiet) {
+            printf("Tile png format is 8-bit grayscale w/ 8-bit alpha\n");
+        }
+        for (uint16_t* rawData = (uint16_t*) sBuffer; rawData < rawDataEnd;) {
+            const uint8_t v = pngData[0];
+            const uint8_t a = pngData[1];
+            const uint8_t c = (v * a / 0xff) >> 3;
+            const uint8_t r = c | (c >> 7);
+            const uint8_t g = (r << 1) | (r >> 4);
+            const uint8_t b = r;
+            const uint16_t hrgb565 = RGB565(r, g, b);
+            *rawData = htole16(hrgb565);
+
+            ++rawData;
+            pngData += 2;
+        }
+        break;
+    }
+
+cleanup:
+    upng_free(upng);
+    return result;
 }
 
 int main(int argc, char *argv[])
@@ -173,7 +490,8 @@ int main(int argc, char *argv[])
                 printf("  partition is a tuple of 5 parameters: type subtype length label binary\n");
                 printf("Version: %s\n\n", kVersion);
                 printf("  description   firmware description; truncated to %d bytes\n", kFirmwareDescriptionSize);
-                printf("  tile          86x48 pixels raw bit pixmap in RGB565 format (%d bytes)\n", kTileSize);
+                printf("  tile          86x48 pixels raw bit pixmap in RGB565 format (%d bytes)\n", kTileByteSize);
+                printf("                or 86x48 pixels png, with .png extension\n");
                 printf("  type          partition type; valid values are:\n");
                 printf("                  app, 0 or 0x00\n");
                 printf("                  data, 1, or 0x01\n");
@@ -282,41 +600,49 @@ int main(int argc, char *argv[])
         }
     }
 
-    {   // block: copy the tile
+    // decode and copy the tile
+    if (endsWith(tileFileName, ".png")) {
+        // got a png file, try to decode it
+        result = decodePng(tileFileName, quiet);
+        if (result != RET_OK) {
+            goto cleanup;
+        }
+    } else {
+        // assume a raw file
         FILE* tileFile = fopen(tileFileName, "rb");
         if (!tileFile) {
             perror("Error");
-            fprintf(stderr, "Tile file not found: %s", tileFileName);
+            fprintf(stderr, "Tile file not found: %s\n", tileFileName);
             RETURN (RET_READ_ERROR);
         }
 
-        const size_t count = fread(sBuffer, 1, kTileSize, tileFile);
-        if (count != kTileSize) {
+        size_t count = fread(sBuffer, 1, kTileByteSize, tileFile);
+        if (count != kTileByteSize) {
             if (feof(tileFile)) {
-                fprintf(stderr, "Tile file is too small. Expected %d bytes, got %zu: %s\n", kTileSize, count, tileFileName);
+                fprintf(stderr, "Tile file is too small. Expected %d bytes, got %zu: %s\n", kTileByteSize, count, tileFileName);
                 fclose(tileFile);
                 RETURN (RET_INVALID_PARAMS);
             }
             perror("Error");
-            fprintf(stderr, "Could not read tile file: %s", tileFileName);
+            fprintf(stderr, "Could not read tile file: %s\n", tileFileName);
             fclose(tileFile);
             RETURN (RET_READ_ERROR);
         }
         if (fgetc(tileFile) != EOF) {
-            fprintf(stderr, "Tile file is too large. Expected %d bytes: %s\n", kTileSize, tileFileName);
+            fprintf(stderr, "Tile file is too large. Expected %d bytes: %s\n", kTileByteSize, tileFileName);
             fclose(tileFile);
             RETURN (RET_INVALID_PARAMS);
         }
         fclose(tileFile);
+    }
 
-        if (!writeAndCrc(sBuffer, kTileSize, file, &checksum)) {
-            perror("Error");
-            fprintf(stderr, "Could not write tile to firmware file: %s\n", firmwareName);
-            RETURN (RET_WRITE_ERROR);
-        }
-        if (!quiet) {
-            printf("Tile: wrote %zu bytes.\n", count);
-        }
+    if (!writeAndCrc(sBuffer, kTileByteSize, file, &checksum)) {
+        perror("Error");
+        fprintf(stderr, "Could not write tile to firmware file: %s\n", firmwareName);
+        RETURN (RET_WRITE_ERROR);
+    }
+    if (!quiet) {
+        printf("Tile: wrote %d bytes.\n", kTileByteSize);
     }
 
     int partCount = 0;
